d20bc4ae36f066032f768487160c9efbad1db831
Bram van den Heuvel <bram.vdh1999@gmail.com>
2023-02-08 16:53:17 +0100

    elm-format

--- src/Internal/Api/All.elm
+++ src/Internal/Api/All.elm
@@ -7,33 +7,44 @@ import Internal.Api.SendStateKey.Main as SendStateKey
 import Internal.Api.Sync.Main as Sync
 import Internal.Api.Versions.Main as Versions
 
+
 {-| Get a specific event from the Matrix API.
 -}
 getEvent : List String -> GetEvent.EventInput -> GetEvent.EventOutput
-getEvent = GetEvent.getEvent
+getEvent =
+    GetEvent.getEvent
+
 
 {-| Get a list of members who are part of a Matrix room.
 -}
 joinedMembers : List String -> JoinedMembers.JoinedMembersInput -> JoinedMembers.JoinedMembersOutput
-joinedMembers = JoinedMembers.joinedMembers
+joinedMembers =
+    JoinedMembers.joinedMembers
+
 
 {-| Send a message event into a Matrix room.
 -}
 sendMessageEvent : List String -> SendMessageEvent.SendMessageEventInput -> SendMessageEvent.SendMessageEventOutput
-sendMessageEvent = SendMessageEvent.sendMessageEvent
+sendMessageEvent =
+    SendMessageEvent.sendMessageEvent
+
 
 {-| Send a state event into a Matrix room.
 -}
 sendStateEvent : List String -> SendStateKey.SendStateKeyInput -> SendStateKey.SendStateKeyOutput
-sendStateEvent = SendStateKey.sendStateKey
+sendStateEvent =
+    SendStateKey.sendStateKey
+
 
 {-| Get the latest sync from the Matrix API.
 -}
 syncCredentials : List String -> Sync.SyncInput -> Sync.SyncOutput
-syncCredentials = Sync.sync
+syncCredentials =
+    Sync.sync
+
 
 {-| Get all supported versions on the Matrix homeserver.
 -}
 versions : Versions.VersionsInput -> Versions.VersionsOutput
-versions = Versions.getVersions
-
+versions =
+    Versions.getVersions
